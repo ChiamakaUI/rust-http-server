384d14b96980153330e12937d1983de13c389f51
ChiamakaUI <55190142+ChiamakaUI@users.noreply.github.com>
2025-06-30 19:08:04 +0100

    railway config

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ async fn main() {
     println!("Hello, world!");
     let app = create_routes();
 
-    let addr = "0.0.0.0:5000".parse().unwrap();
+    let port = std::env::var("PORT").unwrap_or_else(|_| "5000".to_string());
+    let addr = format!("0.0.0.0:{}", port).parse().unwrap();
     println!("Server running on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
